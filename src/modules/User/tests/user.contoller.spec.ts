80992261bdfb6588ea14c501253658c2191eabd1
Guilherme Afonso <guilherme@DESKTOP-V80M3VU>
2024-02-27 10:38:48 -0300

    test: making user tests

---
+++ src/modules/User/tests/user.contoller.spec.ts
@@ -0,0 +1,92 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from 'src/database/PrismaService';
+import { UserModelDTO } from '../models/userModel';
+import { UserController } from '../user.controller';
+import { UserService } from '../user.service';
+import { mockUser, mockUsers } from './user.mock';
+
+describe('UserController', () => {
+  let controller: UserController;
+  let service: UserService;
+  let prisma: PrismaService;
+
+  const userArray = mockUsers;
+
+  const singleUser = mockUser;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        PrismaService,
+        {
+          provide: UserService,
+          useValue: {
+            create: jest.fn((dto: UserModelDTO) => ({
+              id: Date.now(), // Simulating auto-generated ID
+              ...dto,
+              createdAt: new Date().toISOString(),
+              updatedAt: new Date().toISOString(),
+            })),
+            findAll: jest.fn().mockResolvedValue(userArray),
+            findOne: jest.fn().mockResolvedValue(singleUser),
+            update: jest
+              .fn()
+              .mockImplementation((id: number, dto: UserModelDTO) => ({
+                id,
+                ...dto,
+                updatedAt: new Date().toISOString(), // Simulate update timestamp
+              })),
+            delete: jest.fn().mockResolvedValue({ deleted: true }),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  it('should create a user', async () => {
+    const newUserDto: UserModelDTO = { name: 'New User' };
+    const result = await controller.create(newUserDto);
+    expect(result).toEqual(expect.objectContaining(newUserDto));
+    expect(service.create).toHaveBeenCalledWith(newUserDto);
+  });
+
+  it('should find all users', async () => {
+    await expect(controller.findAll()).resolves.toEqual(userArray);
+    expect(service.findAll).toHaveBeenCalled();
+  });
+
+  it('should find one user', async () => {
+    const id = '1';
+    await expect(controller.findById(id)).resolves.toEqual(singleUser);
+    expect(service.findOne).toHaveBeenCalledWith(Number(id));
+  });
+
+  it('should update a user', async () => {
+    const updatedUserDto: UserModelDTO = { name: 'Updated Name' };
+    const id = 1;
+    await expect(
+      controller.update(id.toString(), updatedUserDto),
+    ).resolves.toEqual({
+      id,
+      ...updatedUserDto,
+      updatedAt: expect.any(String),
+    });
+    expect(service.update).toHaveBeenCalledWith(id, updatedUserDto);
+  });
+
+  it('should remove a user', async () => {
+    const id = '1';
+    await expect(controller.delete(id)).resolves.toEqual({ deleted: true });
+    expect(service.delete).toHaveBeenCalledWith(Number(id));
+  });
+});
---
+++ src/modules/User/tests/user.service.spec.ts
@@ -0,0 +1,66 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserModelDTO } from '../models/userModel';
+import { UserRepository } from '../user.repository';
+import { UserService } from '../user.service';
+import { mockUserRepository } from './user.mock';
+
+describe('UserService', () => {
+  let userService: UserService;
+  let userRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        { provide: UserRepository, useFactory: mockUserRepository },
+      ],
+    }).compile();
+
+    userService = module.get<UserService>(UserService);
+    userRepository = module.get(UserRepository);
+  });
+
+  it('should successfully create a user', async () => {
+    userRepository.create.mockResolvedValue('someUser');
+    expect(userRepository.create).not.toHaveBeenCalled();
+    const createUserDto: UserModelDTO = { name: 'Test User' };
+    const result = await userService.create(createUserDto);
+    expect(userRepository.create).toHaveBeenCalledWith(createUserDto);
+    expect(result).toEqual('someUser');
+  });
+
+  it('should find all users', async () => {
+    userRepository.findAll.mockResolvedValue(['user1', 'user2']);
+    expect(userRepository.findAll).not.toHaveBeenCalled();
+    const result = await userService.findAll();
+    expect(userRepository.findAll).toHaveBeenCalled();
+    expect(result).toEqual(['user1', 'user2']);
+  });
+
+  it('should find one user', async () => {
+    userRepository.findOne.mockResolvedValue('user');
+    const id = 1;
+    expect(userRepository.findOne).not.toHaveBeenCalled();
+    const result = await userService.findOne(id);
+    expect(userRepository.findOne).toHaveBeenCalledWith(id);
+    expect(result).toEqual('user');
+  });
+
+  it('should update a user', async () => {
+    userRepository.findOne.mockResolvedValue('existingUser');
+    userRepository.update.mockResolvedValue('updatedUser');
+    const updateUserDto: UserModelDTO = { name: 'Updated User' };
+    const id = 1;
+    const result = await userService.update(id, updateUserDto);
+    expect(userRepository.update).toHaveBeenCalledWith(id, updateUserDto);
+    expect(result).toEqual('updatedUser');
+  });
+
+  it('should delete a user', async () => {
+    userRepository.findOne.mockResolvedValue('existingUser');
+    userRepository.delete.mockResolvedValue(undefined);
+    const id = 1;
+    await userService.delete(id);
+    expect(userRepository.delete).toHaveBeenCalledWith(id);
+  });
+});
