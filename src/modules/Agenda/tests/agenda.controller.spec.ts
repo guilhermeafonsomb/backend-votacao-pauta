4f536c49209b4c9056861e73f5a885ca6382d67d
Guilherme Afonso <guilherme@DESKTOP-V80M3VU>
2024-02-27 13:13:47 -0300

    test: making agenda tests

---
+++ src/modules/Agenda/tests/agenda.controller.spec.ts
@@ -0,0 +1,92 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from 'src/database/PrismaService';
+import { AgendaDTO } from 'src/modules/models/agendaModels';
+import { AgendaController } from '../agenda.controller';
+import { AgendaService } from '../agenda.service';
+import { mockAgenda, mockAgendaDTO, mockAgendas } from './agenda.mock';
+
+describe('AgendaController', () => {
+  let controller: AgendaController;
+  let service: AgendaService;
+  let prisma: PrismaService;
+
+  const agendaArray = mockAgendas;
+
+  const singleAgendar = mockAgenda;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AgendaController],
+      providers: [
+        PrismaService,
+        {
+          provide: AgendaService,
+          useValue: {
+            create: jest.fn((dto: AgendaDTO) => ({
+              id: Date.now(), // Simulating auto-generated ID
+              ...dto,
+              createdAt: new Date().toISOString(),
+              updatedAt: new Date().toISOString(),
+            })),
+            findAll: jest.fn().mockResolvedValue(mockAgendas),
+            findOne: jest.fn().mockResolvedValue(singleAgendar),
+            update: jest
+              .fn()
+              .mockImplementation((id: number, dto: AgendaDTO) => ({
+                id,
+                ...dto,
+                updatedAt: new Date().toISOString(), // Simulate update timestamp
+              })),
+            delete: jest.fn().mockResolvedValue({ deleted: true }),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<AgendaController>(AgendaController);
+    service = module.get<AgendaService>(AgendaService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  it('should create a agenda', async () => {
+    const newAgendaDto: AgendaDTO = mockAgendaDTO;
+    const result = await controller.create(newAgendaDto);
+    expect(result).toEqual(expect.objectContaining(newAgendaDto));
+    expect(service.create).toHaveBeenCalledWith(newAgendaDto);
+  });
+
+  it('should find all agendas', async () => {
+    await expect(controller.findAll()).resolves.toEqual(agendaArray);
+    expect(service.findAll).toHaveBeenCalled();
+  });
+
+  it('should find one agenda', async () => {
+    const id = '1';
+    await expect(controller.findOne(id)).resolves.toEqual(singleAgendar);
+    expect(service.findOne).toHaveBeenCalledWith(Number(id));
+  });
+
+  it('should update a agenda', async () => {
+    const updatedAgendaDto: AgendaDTO = mockAgendaDTO;
+    const id = 1;
+    await expect(
+      controller.update(id.toString(), updatedAgendaDto),
+    ).resolves.toEqual({
+      id,
+      ...updatedAgendaDto,
+      updatedAt: expect.any(String),
+    });
+    expect(service.update).toHaveBeenCalledWith(id, updatedAgendaDto);
+  });
+
+  it('should remove a agenda', async () => {
+    const id = '1';
+    await expect(controller.delete(id)).resolves.toEqual({ deleted: true });
+    expect(service.delete).toHaveBeenCalledWith(Number(id));
+  });
+});
---
+++ src/modules/Agenda/tests/agenda.service.spec.ts
@@ -0,0 +1,66 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AgendaDTO } from 'src/modules/models/agendaModels';
+import { AgendaRepository } from '../agenda.repository';
+import { AgendaService } from '../agenda.service';
+import { mockAgendaDTO, mockAgendaRepository } from './agenda.mock';
+
+describe('AgendaService', () => {
+  let agendaService: AgendaService;
+  let agendaRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AgendaService,
+        { provide: AgendaRepository, useFactory: mockAgendaRepository },
+      ],
+    }).compile();
+
+    agendaService = module.get<AgendaService>(AgendaService);
+    agendaRepository = module.get(AgendaRepository);
+  });
+
+  it('should successfully create a agenda', async () => {
+    agendaRepository.create.mockResolvedValue('someAgenda');
+    expect(agendaRepository.create).not.toHaveBeenCalled();
+    const createAgendaDto: AgendaDTO = mockAgendaDTO;
+    const result = await agendaService.create(createAgendaDto);
+    expect(agendaRepository.create).toHaveBeenCalledWith(createAgendaDto);
+    expect(result).toEqual('someAgenda');
+  });
+
+  it('should find all agendas', async () => {
+    agendaRepository.findAll.mockResolvedValue(['agenda1', 'agenda2']);
+    expect(agendaRepository.findAll).not.toHaveBeenCalled();
+    const result = await agendaService.findAll();
+    expect(agendaRepository.findAll).toHaveBeenCalled();
+    expect(result).toEqual(['agenda1', 'agenda2']);
+  });
+
+  it('should find one agenda', async () => {
+    agendaRepository.findOne.mockResolvedValue('agenda');
+    const id = 1;
+    expect(agendaRepository.findOne).not.toHaveBeenCalled();
+    const result = await agendaService.findOne(id);
+    expect(agendaRepository.findOne).toHaveBeenCalledWith(id);
+    expect(result).toEqual('agenda');
+  });
+
+  it('should update a agenda', async () => {
+    agendaRepository.findOne.mockResolvedValue('existingagenda');
+    agendaRepository.update.mockResolvedValue('updatedagenda');
+    const updateagendaDto: AgendaDTO = mockAgendaDTO;
+    const id = 1;
+    const result = await agendaService.update(id, updateagendaDto);
+    expect(agendaRepository.update).toHaveBeenCalledWith(id, updateagendaDto);
+    expect(result).toEqual('updatedagenda');
+  });
+
+  it('should delete a agenda', async () => {
+    agendaRepository.findOne.mockResolvedValue('existingAgenda');
+    agendaRepository.delete.mockResolvedValue(undefined);
+    const id = 1;
+    await agendaService.delete(id);
+    expect(agendaRepository.delete).toHaveBeenCalledWith(id);
+  });
+});
