00954ba604e035d5f3e79e57990a229e9b1dc46a
Guilherme Afonso <guilherme@DESKTOP-V80M3VU>
2024-02-28 10:59:39 -0300

    wip: create user and fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
