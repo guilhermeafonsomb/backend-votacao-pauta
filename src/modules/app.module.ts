3395fbac7f0003be10db8328775dff51398f1936
Guilherme Afonso <guilherme@DESKTOP-V80M3VU>
2024-03-03 16:52:23 -0300

    refact: adding category module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AgendaModule } from './Agenda/agenda.module';
+import { CategoryModule } from './Category/category.module';
 import { UserModule } from './User/user.module';
 import { VoteModule } from './Vote/vote.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [UserModule, AgendaModule, VoteModule],
+  imports: [UserModule, AgendaModule, VoteModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
