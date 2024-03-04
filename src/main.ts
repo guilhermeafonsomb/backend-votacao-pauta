bd526a2f9963f0b116d1289beee98b84f211cb23
Guilherme Afonso <guilherme@DESKTOP-V80M3VU>
2024-03-04 00:00:32 -0300

    feat: improve log api running

--- src/main.ts
+++ src/main.ts
@@ -10,5 +10,6 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+  Logger.log(`📚 Documentation is running on: http://localhost:${port}/api#/`);
 }
 bootstrap();
